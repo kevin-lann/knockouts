84da78d8c96effe17b2ba325d97440e66bea9216
Beself Lan <beselflan@gmail.com>
2026-02-22 22:27:33 -0500

    fixed eslint formatting issues in validation.ts

--- game/src/utils/validation.ts
+++ game/src/utils/validation.ts
@@ -21,18 +21,18 @@ export function getLevenshteinThreshold(input: string): number {
  */
 export function validateAnswer(
   userInput: string,
-  validAnswers: Answer[],
+  validAnswers: Answer[]
 ): Answer | null {
   const normalizedInput = normalize(userInput)
 
   return (
     validAnswers.find((answer) =>
-      answer.variants.some((variant) => normalize(variant) === normalizedInput),
+      answer.variants.some((variant) => normalize(variant) === normalizedInput)
     ) ||
     validAnswers.find(
       (answer) =>
         levenshtein(normalizedInput, normalize(answer.display_text)) <=
-        getLevenshteinThreshold(normalize(answer.display_text)),
+        getLevenshteinThreshold(normalize(answer.display_text))
     ) ||
     null
   )
@@ -43,7 +43,7 @@ export function validateAnswer(
  * Returns Set of player IDs who submitted duplicate answers
  */
 export function findDuplicates(
-  validatedAnswers: Map<string, Answer | null>,
+  validatedAnswers: Map<string, Answer | null>
 ): Set<string> {
   // NOTE: duplicate answers that are not correct will not be flagged
   const duplicates = new Set<string>()
@@ -51,7 +51,7 @@ export function findDuplicates(
 
   // Group players by validated answer
   for (const [playerId, answer] of validatedAnswers) {
-    let answer_text = answer?.display_text
+    const answer_text = answer?.display_text
     if (!answer_text) continue
 
     if (!answers.has(answer_text)) {
